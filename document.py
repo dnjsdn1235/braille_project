49b475a7afc876ceecc96c1d9585a12ec6e4d3b8
Kim MinJae <alswo7310@naver.com>
2019-06-26 00:52:39 +0900

    Update document.py
    Edited test doc to use string atomizer instead of existing other parsing libraries.

--- document.py
+++ document.py
@@ -1,34 +1,16 @@
--*- coding: utf-8 -*-
 try:
     import Image
 except ImportError:
     from PIL import Image
 import pytesseract
-import argparse
-from jamostoolkit import JamosSeparator
-
-abcd=pytesseract.image_to_string(Image.open('hangul.png'), lang='kor') # OCR
-
-parser = argparse.ArgumentParser(description='Jamos toolkit')
-parser.add_argument('--string', default=abcd, help='Please enter the string you want to separator.$
-args = parser.parse_args()
+from string_atomizer import StringAtomizer
 
 def main():
-    jamos = JamosSeparator(args.string)
-    jamos.run()
-    print(format(args.string))
-    print(format(jamos.get()))
+    abcd=pytesseract.image_to_string(Image.open('hangul.png'), lang='kor') # OCR
+    sa = StringAtomizer()
+    result = sa.atomizeString(abcd)
     
-    print(format(jamos.get())[1])
-    print(format(jamos.get())[2])
-    print(format(jamos.get())[3])
-    print(format(jamos.get())[4])
-    print(format(jamos.get())[5])
-    print(format(jamos.get())[6])
-    print(format(jamos.get())[7])
-    print(format(jamos.get())[8])
-    print(format(jamos.get())[9])
-    print(format(jamos.get())[10])
+    print(result)
 
 if __name__ == '__main__':
     main()
